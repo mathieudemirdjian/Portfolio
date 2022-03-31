82f9e915fcd55c7a35beb5e7e0de5fffb48402b2
Mathieu Demirdjian <mathieu.demirdjian@gmail.com>
2022-03-31 18:04:38 +0200

    fix bug, add front form verification

--- js/index.js
+++ js/index.js
@@ -1,31 +1,7 @@
-// const base = {
-//     closeNav: function() {
-//         const navElement = document.querySelector('.navigation');
-//         var w = document.documentElement.clientWidth || window.innerWidth;
-
-//         if (w <= 1200) {
-//             navElement.remove();
-//          }
-//          console.log(w);
-//     },
-    
-//     init: function () { 
-//         base.closeNav();
-
-//         console.log('init !');
-//     },
-// }
-
-// document.addEventListener('DOMContentLoaded', base.init);
-
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.dropdown-menu');
 
-burger.addEventListener('click', (e) => {
-    // if ((!burger.contains(e.target)) || (!menu.contains(e.target))){
-    //     burger.classList.toggle('active')
-    //     menu.classList.toggle('dropdown-menu-active')
-    // }
+burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     menu.classList.toggle('dropdown-menu-active')
 });
@@ -45,7 +21,41 @@ const switchBox = document.querySelector(".sun-moon");
 switchBox.addEventListener("click", () => {
     switchBox.classList.toggle('move')
     body.classList.toggle('daymode')
-    
 });
 
+const nom = document.getElementById('nom');
+const email = document.getElementById('email');
+const texte = document.getElementById('texte');
+const form = document.getElementById('form');
+const erreur = document.getElementById('erreur');
+console.log(nom);
+console.log(texte);
+form.addEventListener('submit', (e) => {
+    console.log(e);
+    let messages = [];
+    console.log(messages);
+    const emailcheck = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   
+    if (nom.value.trim() === '' || nom.value == null) {
+        console.log(nom.value);
+        messages.push('Veuillez entrer votre nom');
+    }
+
+    if(emailcheck.test(email.value.trim())) {
+        console.log(emailcheck.value);
+    }else {
+        messages.push('Veuillez entrer un email valide');
+    } 
+
+    if (texte.value.trim() === '' || texte.value == null) {
+        console.log(texte.value);
+        messages.push('Veuillez entrer votre message');
+    } 
+
+    if (messages.length > 0) {
+        e.preventDefault();
+        erreur.innerHTML = messages.join('<br>');
+    }
+
+})
 
